9ce8a96009b7ce4c7a08152865b216d27657f547
lshaoqin <shaoqin.lu@gmail.com>
2023-12-01 13:47:49 +0800

    add solution for day 1.1

---
+++ day1.1/solution.py
@@ -0,0 +1,23 @@
+f = open("day1.1/input.txt", "r")
+lines = f.readlines()
+
+def extract_value(line):
+    first = 0
+    last = 0
+    for char in line:
+        if char >= '0' and char <= '9':
+            first = int(char)
+            break
+
+    for char in line[::-1]:
+        if char >= '0' and char <= '9':
+            last = int(char)
+            break
+    
+    return first * 10 + last
+
+sum = 0
+for line in lines:
+    sum += extract_value(line)
+
+print(sum)
